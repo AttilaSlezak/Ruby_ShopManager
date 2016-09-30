20f7ab59669ff25d6fdc5e4cca9d2a0a714ca67d
Slezak Attila <attila.slezak@gmail.com>
2016-09-30 01:19:51 +0100

    Create LongLifeMilk and SemiLongLifeMilk classes as child classes of Milk class - Part III - 2

---
+++ Shop/semi_long_life_milk.rb
@@ -0,0 +1,7 @@
+class SemiLongLifeMilk < Milk
+
+  def initialize(barcode, cubic_capacity, producer, best_before, fat_content)
+    super(barcode, cubic_capacity, producer, best_before, fat_content)
+  end
+
+end
